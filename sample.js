551f508bc31a3735e74a2130177f169e12fd83f6
bhavya-alanka <Saibhavyasri8@gmail.com>
2025-09-06 12:37:49 +0530

    commands

--- sample.js
+++ sample.js
@@ -1,1 +1,28 @@
 console.log("this is a sample file")
+let name="bhavya";
+console.log(name)
+let a=3;
+let b=4;
+console.log(a+b);
+console.log(typeof(name));
+let s=false
+console.log(typeof(s));
+let x
+console.log(x)
+console.log(typeof(x));
+let y=null;
+console.log(typeof(y));
+let array=["surya",16,true,"bha"];
+console.log(array)
+console.log(array.length)
+console.log(array[1])
+let details=
+{
+    name:"bhavya",
+    age:3,
+    address:"hyderabad"
+
+};
+console.log(details);
+console.log(details.name);
+console.log(typeof(details));
