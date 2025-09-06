680ab317cce4acd5ea3fda43d3766ac115a57a9b
bhavya-alanka <Saibhavyasri8@gmail.com>
2025-09-06 16:57:34 +0530

    export

---
+++ math.js
@@ -0,0 +1,17 @@
+  const add=function (a,b){
+    return a+b;
+}
+
+ 
+const multiply=function (c,d)
+{
+    return c*d;
+}
+
+
+const subt=function (e,f){
+    return e-f;
+}
+
+export default subt 
+export {add,multiply}
