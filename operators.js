7f5a2522ee3278046dfa4b13fe94f2576f5f35cc
bhavya-alanka <Saibhavyasri8@gmail.com>
2025-09-06 14:58:46 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,46 @@
+let a=10;
+let b=5;
+console.log("arthimetic operators")
+console.log("the sum is ",a+b)
+console.log("the difference is",a-b)
+console.log("the multiplication is",a*b)
+console.log("the division is",a/b)
+console.log("the power is",a**b)
+console.log("the modular is",a%b)
+  
+console.log("relational operators")
+console.log(a>b)
+console.log(a<b)
+
+let c=5
+let d="bhavya"
+console.log("comparison operators")
+console.log(c==d)
+console.log(a==b)
+console.log(c===d)
+
+console.log("assignment operators")
+console.log(a+=2)
+
+console.log("logical operators")
+console.log(a>b && c!==d)
+console.log(a<b || c==d)
+
+console.log("string operators")
+//camelcase
+Firstname="bhavya"
+Lastname="vatti"
+output=Firstname+" "+Lastname;
+console.log(output)
+ 
+let Correctname=`hello ${Firstname} ${Lastname}, welcome!`
+console.log(Correctname)
+
+console.log("ternary operator")
+//instead of if statements , we are using ternary operators
+output1=(a>b) ? "a is greator":"b is greator";
+console.log(output1)
+
+console.log("increment or decrement operators")
+console.log(a++)
+console.log(--b)
